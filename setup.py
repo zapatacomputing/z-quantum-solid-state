3cb1b561d2d409aa221c50ae2c41c4d37dc96247
mstechly <michal.stechly@gmail.com>
2021-04-14 08:31:06 -0400

    Moved setup.py to main dir.

--- setup.py
+++ setup.py
@@ -14,14 +14,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zapatacomputing/z-quantum-solid-state ",
-    packages=setuptools.find_namespace_packages(include=['zquantum.*']),
-    package_dir={'' : 'python'},
+    packages=setuptools.find_namespace_packages(include=["zquantum.*"]),
+    package_dir={"": "python"},
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        'z-quantum-core',
-        'openfermion'
-    ]
+        "z-quantum-core",
+    ],
 )
