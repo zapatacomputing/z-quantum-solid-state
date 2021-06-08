57f5103d49ceb41dad63fa26f100d9f48599412c
simonwa7 <simonwa7@gmail.com>
2021-06-08 12:20:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-import setuptools
 import os
 
-readme_path = os.path.join("README.md")
-with open(readme_path, "r") as f:
-    long_description = f.read()
+import setuptools
+
+dev_requires = [
+    "pytest>=3.7.1",
+    "pytest-cov>=2.5.1",
+    "tox>=3.2.1",
+    "flake8>=3.7.9",
+    "black>=19.3b0",
+    "pre_commit>=2.10.1",
+]
+
+extras_require = {
+    "dev": dev_requires,
+}
+
+
+def _this_path():
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def _read_readme():
+    with open(os.path.join(_this_path(), "README.md")) as f:
+        return f.read()
+
 
 setuptools.setup(
     name="z-quantum-solid-state",
@@ -11,16 +31,19 @@ setuptools.setup(
     author="Zapata Computing, Inc.",
     author_email="info@zapatacomputing.com",
     description="Library for solid state calculations for Orquestra.",
-    long_description=long_description,
+    license="Apache-2.0",
+    long_description=_read_readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/zapatacomputing/z-quantum-solid-state ",
-    packages=setuptools.find_namespace_packages(include=["zquantum.*"]),
-    package_dir={"": "src/python"},
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+    url="https://github.com/zapatacomputing/z-quantum-core",
+    packages=setuptools.find_namespace_packages(
+        include=["zquantum.*"], where="src/python"
     ),
-    install_requires=[
-        "z-quantum-core",
+    package_dir={"": "src/python"},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
     ],
+    install_requires=["z-quantum-core"],
+    extras_require=extras_require,
 )
